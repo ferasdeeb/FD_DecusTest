4fd651ac637bd49be5ac062740ab187c92c67a8d
Feras Dib <ferasdeeb@gmail.com>
2026-05-05 14:52:54 +0100

    Added some testing comments. To be revisited after proposing a new project structure

---
+++ DecusTest.Business.Tests/RaterCreateOptionTests.cs
@@ -0,0 +1,37 @@
+﻿using DecusTest.Business.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace DecusTest.Business.Tests
+{
+
+    // Note for interview: This test class is created to demonstrate the requirement to create a unit test for the existing codebase.
+    // This is following the same logic/structure of the existing test samples as created in RaterCreateOptionTests
+
+    // Ideally we should one TestClass called RaterTests, and within this class we should have unit tests for all the public methods of the Rater class.
+
+
+    [TestClass]
+    public class RaterCreateOptionTests
+    {
+        RiskData _riskData;
+        RiskOption _riskOption;
+        List<object> _coverageOptions;
+
+        [TestInitialize] 
+        public void Initialize() 
+        {
+            _riskData = new RiskData();
+            _riskOption = new RiskOption();
+            _coverageOptions = new List<object>();
+        }
+
+        [DataTestMethod]
+        [DataRow(nameof(RiskData.OptionalCoverageD))]
+        public void CreateOption_ValuesSetCorrectly(string coverageName)
+        {
+
+
+        }
+    }
+}
--- DecusTest.Business.Tests/RaterSetRiskPropertyValueTests.cs
+++ DecusTest.Business.Tests/RaterSetRiskPropertyValueTests.cs
@@ -6,44 +6,54 @@ namespace DecusTest.Business.Tests
     [TestClass]
     public class RaterSetRiskPropertyValueTests
     {
+
+        RiskData _riskData;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _riskData = new RiskData();
+        }
+
         [DataTestMethod]
         [DataRow(nameof(RiskData.TotalInsuredValue), 5000.0)]
-        [DataRow(nameof(RiskData.TotalInsuredValue), 12345.67)]
+        [DataRow(nameof(RiskData.TotalInsuredValue), 12345.67)] // Note for interview: This test is not needed since the test has been covered in the previous sample. The same applies to the tests below.
         public void SetRiskPropertyValue_ShouldSetDoubleProperties(string propertyName, double value)
         {
-            var riskData = new RiskData();
-            Rater.SetRiskPropertyValue(riskData, propertyName, value);
-            Assert.AreEqual(value, riskData.TotalInsuredValue);
+            Rater.SetRiskPropertyValue(_riskData, propertyName, value);
+            Assert.AreEqual(value, _riskData.TotalInsuredValue);
         }
 
         [DataTestMethod]
         [DataRow(nameof(RiskData.State), "FL")]
-        [DataRow(nameof(RiskData.State), "TX")]
+        [DataRow(nameof(RiskData.State), "TX")] // Not needed since the test has been covered in the previous sample
+        [DataRow(nameof(RiskData.State), " ")]
+        [DataRow(nameof(RiskData.State), null)]
+        [DataRow(nameof(RiskData.State), "")]
         public void SetRiskPropertyValue_ShouldSetStringProperties(string propertyName, string value)
         {
-            var riskData = new RiskData();
-            Rater.SetRiskPropertyValue(riskData, propertyName, value);
-            Assert.AreEqual(value, riskData.State);
+            Rater.SetRiskPropertyValue(_riskData, propertyName, value);
+            Assert.AreEqual(value, _riskData.State);
         }
 
         [DataTestMethod]
         [DataRow(nameof(RiskData.OptionalCoverageA), true)]
         [DataRow(nameof(RiskData.OptionalCoverageA), false)]
+        [DataRow(nameof(RiskData.OptionalCoverageA), null)]
         public void SetRiskPropertyValue_ShouldSetBoolProperties(string propertyName, bool value)
         {
-            var riskData = new RiskData();
-            Rater.SetRiskPropertyValue(riskData, propertyName, value);
-            Assert.AreEqual(value, riskData.OptionalCoverageA);
+            Rater.SetRiskPropertyValue(_riskData, propertyName, value);
+            Assert.AreEqual(value, _riskData.OptionalCoverageA);
         }
 
         [DataTestMethod]
         [DataRow(nameof(RiskData.OptionalCoverageB), 10.0)]
-        [DataRow(nameof(RiskData.OptionalCoverageB), 20.0)]
+        [DataRow(nameof(RiskData.OptionalCoverageB), 20.0)] // Not needed since the test has been covered in the previous sample
+        [DataRow(nameof(RiskData.OptionalCoverageB), null)]
         public void SetRiskPropertyValue_ShouldSetNullableDoubleProperties(string propertyName, double value)
         {
-            var riskData = new RiskData();
-            Rater.SetRiskPropertyValue(riskData, propertyName, value);
-            Assert.AreEqual(value, riskData.OptionalCoverageB);
+            Rater.SetRiskPropertyValue(_riskData, propertyName, value);
+            Assert.AreEqual(value, _riskData.OptionalCoverageB);
         }
 
         [DataTestMethod]
@@ -51,9 +61,8 @@ namespace DecusTest.Business.Tests
         [DataRow(nameof(RiskData.OptionalCoverageC), "Custom Value")]
         public void SetRiskPropertyValue_ShouldSetStringCoverageProperties(string propertyName, string value)
         {
-            var riskData = new RiskData();
-            Rater.SetRiskPropertyValue(riskData, propertyName, value);
-            Assert.AreEqual(value, riskData.OptionalCoverageC);
+            Rater.SetRiskPropertyValue(_riskData, propertyName, value);
+            Assert.AreEqual(value, _riskData.OptionalCoverageC);
         }
     }
 }
